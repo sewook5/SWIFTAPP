1a2828ca78e5177bc58277b5ad4a4ad3133c1d05
Matthew Oh <sewook5@gmail.com>
2017-05-17 11:39:52 -0500

    Textfield

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet var theLabel: UILabel!
+ 
+    @IBOutlet var Text1: UITextField!
+    @IBOutlet var Text2: UITextField!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
 
+        print(Text1.text!)
+        print(Text2.text!)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
